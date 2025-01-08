6d752c883fa7c5c2ae63a1eeff1f687e1f3a31eb
Nicolas <nicolasdelfino585@gmail.com>
2025-01-08 14:55:21 -0300

    num 7 done

--- exercises/006-getFullName/app.js
+++ exercises/006-getFullName/app.js
@@ -1,4 +1,6 @@
+/* Escribe una función llamada getFullName. Dado un nombre y un apellido, getFullName retorna un string con el nombre y apellido separados por un espacio. */
+
 function getFullName(firstName, lastName) {
   // your code here
-  
+  return `${firstName}  ${lastName}`;
 }
--- exercises/007-getLengthOfWord/app.js
+++ exercises/007-getLengthOfWord/app.js
@@ -1,4 +1,10 @@
+/* Escribe una función llamada getLengthOfWord. Dada un palabra, getLengthOfWord debe retornar la cantidad de letras que tiene esa palabra. */
+
 function getLengthOfWord(word) {
   // your code here
-  
+  let contador = 0;
+  for (i = 0; i < word.length; i++) {
+    contador += 1;
+  }
+  return contador;
 }
