74643fa323a41f5aabad610953f5ea1142a8c6df
Nicolas <nicolasdelfino585@gmail.com>
2025-05-21 12:24:53 -0300

    estudiar filter repaso de ejercicios

--- exercises/097-getElementsGreaterThanTenAtProperty/app.js
+++ exercises/097-getElementsGreaterThanTenAtProperty/app.js
@@ -1,1 +1,7 @@
 // Write your function here
+function getElementsGreaterThan10AtProperty(obj, clave) {
+  if (Array.isArray(obj[clave])) {
+    return obj[clave].filter((element) => element > 10);
+  }
+  return [];
+}
