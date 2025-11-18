06dc0c619f20881d773c955786e5c3155a265d02
Nicolas <nicolasdelfino585@gmail.com>
2025-11-18 18:24:17 -0300

    mas logica diaria

--- exercises/135-computeFactorialOfN/app.js
+++ exercises/135-computeFactorialOfN/app.js
@@ -1,13 +1,7 @@
 function computeFactorialOfN(n) {
     // your code here
 
-    total = 1
-    for (let i = 1; i <= n; i++) {
-        total = total * i
-
-    }
-    return total
-
+   
 }
 
 let output = computeFactorialOfN(4);
