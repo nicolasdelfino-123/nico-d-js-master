0235c72e0ef872e5eabca4dea29ef12bff683e70
Nicolas <nicolasdelfino585@gmail.com>
2025-11-18 18:17:21 -0300

    practica diaria js done

--- exercises/136-sumDigits/app.js
+++ exercises/136-sumDigits/app.js
@@ -1,6 +1,18 @@
 function sumDigits(num) {
     // your code here
-    
+    let str = num.toString()
+
+    let total = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (i === 0 && str[i] === "-") {
+            total -= Number(str[i + 1])
+            i++
+        } else {
+            total += Number(str[i])
+        }
+
+    }
+    return total
 }
 
 let output = sumDigits(-316);
